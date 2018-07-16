97c3685de1526c977df40e13b881d3fad8702b43
Jorge Visca <jvisca@fing.edu.uy>
2018-07-16 14:59:11 -0300

    robotito script

--- robotito.lua
+++ robotito.lua
@@ -1,3 +1,5 @@
+--dofile('robotito.lua')
+
 -- { {xshutpin, [newadddr]}, ... }
 local sensors = {
  {16},
@@ -8,6 +10,23 @@ local sensors = {
  {13},
 }
 local omni_conf = {5.0, 27,26, 33,25, 2,15}
+
+local ledpin = pio.GPIO19
+local n_pins = 24
+
+local max_bright = 70
+local colors = {
+ {max_bright, 0, 0},
+ {max_bright/2, max_bright/2, 0},
+ {0, max_bright, 0},
+ {0, max_bright/2, max_bright/2},
+ {0, 0, max_bright},
+ {max_bright/2, 0, max_bright/2},
+}
+
+local first_led = {0, 20, 16, 12, 8, 4}
+
+neo = neo or neopixel.attach(neopixel.WS2812B, ledpin, n_pins)
 
 local ms = ms or 100  -- period of distance measurements
 
@@ -24,20 +43,55 @@ assert(omni.init(table.unpack(omni_conf)))
 
 button = sensor.attach("PUSH_SWITCH", pio.GPIO0)
 
+
+local sin60 = math.sqrt(3)/2
+local dmin = 100
+local dmax = 250
+local norm_x = 100 / math.sqrt(3)
+local norm_y = 100 / 2
+
+local floor = math.floor
+
 -- the callback will be called with all sensor readings
 local dist_callback= function(d1, d2, d3, d4, d5, d6)
   --print('dist:', d1, d2, d3, d4, d5, d6)
+  local d={d1, d2, d3, d4, d5, d6}
 
-  --calcular vector
-  local v
-  if d1<100 then v=0 
-  elseif d1>200 then v=100
-  else v=d1-100 end
+  --print('dist:', table.unpack(d))
 
-  print(d1, v)
 
-  --mandar a omni
-  omni.raw_write(v)
+  for i=1,6 do
+    if d[i]<dmin or d[i]>dmax then 
+      d[i]=0
+      
+      for j = 0, 3 do
+          neo:setPixel(first_led[i]+j, 0,0,0)
+      end
+      neo:update()
+      
+    else
+      --d[i]=dmax-d[i]
+      d[i] = (dmax-d[i]) / (dmax-dmin)   -- 0..1
+      
+      local color = {
+        floor(d[i]*colors[i][1]),
+        floor(d[i]*colors[i][2]),
+        floor(d[i]*colors[i][3])
+      }
+      for j = 0, 3 do
+        neo:setPixel(first_led[i]+j, table.unpack(color))
+      end
+      neo:update()
+      
+    end
+  end
+
+  local xdot = ( (d[3]+d[2]-d[6]-d[5])*sin60 ) *norm_x
+  local ydot = ( (d[3]+d[5]-d[2]-d[6])/2 + d[4]-d[1] ) *norm_y
+
+  print(xdot, ydot, '', 'dist:', table.unpack(d))
+  --omni.drive(xdot,ydot,0)
+  
 end
 
 local function button_callback(data)
@@ -59,7 +113,15 @@ end
 
 button:callback(button_callback)
 
--- run for 60 seconds
---tmr.sleepms(60*1000)
+print("on")
+omni.set_enable()
+vlring.get_continuous(ms, dist_callback)
 
+--[[
+tmr.sleepms(20*1000)
 
+print("off")
+vlring.get_continuous(false)
+omni.set_enable(false)
+--vlring.release()
+--]]
