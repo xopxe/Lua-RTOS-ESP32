c72e17bdb76560d4329e1c0f475a18d8373f2a69
gamorin <guillermoamorin@gmail.com>
2018-08-06 16:01:41 -0300

    lua library with initialization

--- omni.lua
+++ omni.lua
@@ -1,23 +1,27 @@
---WHEEL_DIAMETER   = 0.038   --m
+local WHEEL_DIAMETER   = 0.038   --m
 --WHEEL_PERIMETER = WHEEL_DIAMETER*3.141592654
 local ENC_CPR = 12  --counts per revolution
 local MOTOR_REDUCTION = 50
 local TICS_PER_REVOLUTION = ENC_CPR*MOTOR_REDUCTION
 local RAD_PER_TICK = 2*math.pi / TICS_PER_REVOLUTION
 
+local MAX_SPEED_POWER = 90 -- power % at which MAX_SPEED_TICS value is obtained
+local MAX_SPEED_TICS = 1080 --tics/s at MAX_SPEED_POWER
+
+local MAX_SPEED_RAD = MAX_SPEED_TICS * RAD_PER_TICK  -- rad/s
+local MAX_SPEED_LIN = MAX_SPEED_RAD * WHEEL_DIAMETER / 2 -- m/s
+
 -- forward feed parameter
--- at 90% does about 1080 tics/s
-local KF = 90/1080     --ENC_CPR*90/1080
+local KF = MAX_SPEED_POWER / MAX_SPEED_LIN
 
 local omni = require('omni_hbridge')
 
-local KP = 0.1     --0.1/ENC_CPR
+local KP = 0.0     --0.1/ENC_CPR
 
 -- initialize with tobot radius and drivers' pins
 omni.init(5.0, 27,26,39,37, 33,25,38,36, 23,18,34,35)
 omni.set_pid(KP, 0.0, 0.0, KF)
+omni.set_max_output(80.0)
 
 
 return omni
-
-
